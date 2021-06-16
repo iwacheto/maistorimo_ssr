146c5e109c62c96f0c1d5a97c9d7e51aa5033542
gprusiyski <g.prusiyski@webrika.bg>
2021-06-16 15:33:34 +0000

    fix

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel + React server side rendering example</title>
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}?ver=13">
-        <script defer src="{{ mix('js/react/entry-client.js') }}?ver=13"></script>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}?ver=14">
+        <script defer src="{{ mix('js/react/entry-client.js') }}?ver=14"></script>
     </head>
     <body class="bg-paper font-sans leading-normal text-grey-darkest border-t-4 border-orange-light">
 
